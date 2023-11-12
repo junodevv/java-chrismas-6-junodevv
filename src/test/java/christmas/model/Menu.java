af6cfe37a2fabd1e2324b991fa77ba2715bed54e
leejuno <zhqkfxm0127@naver.com>
2023-11-12 20:45:59 +0900

    feat(christmas): Menu enum 클래스 구현

---
+++ src/test/java/christmas/model/Menu.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+public enum Menu {
+    YANGSONG_SOUP("appetizer", "양송이수프", 6000),
+    TAPAS("appetizer", "타바스", 5500),
+    CAESAR_SALAD("appetizer", "시저샐러드", 8000),
+
+    T_BONE_STEAK("main", "티본스테이크", 55000),
+    BBQ_RIB("main", "비비큐립", 54000),
+    SEAFOOD_PASTA("main", "해산물스타", 35000),
+    CHRISTMAS_PASTA("main", "크리스마스파스타", 25000),
+
+    CHOCO_CAKE("dessert", "초코케이크", 15000),
+    ICE_CREAM("dessert", "아이스크림", 5000),
+
+    ZERO_COLA("drink", "제로콜라", 3000),
+    RED_WINE("drink", "레드와인", 60000),
+    CHAMPAGNE("drink", "샴페인", 25000);
+
+    private String type;
+    private String name;
+    private int price;
+
+    Menu(String type, String name, int price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+}
