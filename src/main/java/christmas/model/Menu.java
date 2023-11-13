163dae133fcc6e266f7eb84241ee3bc4e6d65c4b
leejuno <zhqkfxm0127@naver.com>
2023-11-13 15:46:52 +0900

    refactor(Menu): 오타수정

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -6,7 +6,7 @@ public enum Menu {
     CAESAR_SALAD("appetizer", "시저샐러드", 8000),
 
     T_BONE_STEAK("main", "티본스테이크", 55000),
-    BBQ_RIB("main", "비비큐립", 54000),
+    BBQ_RIB("main", "바비큐립", 54000),
     SEAFOOD_PASTA("main", "해산물스타", 35000),
     CHRISTMAS_PASTA("main", "크리스마스파스타", 25000),
 
