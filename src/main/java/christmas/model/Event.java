d34b21d80545b5dc5fc09426fae1f954cca37cc9
leejuno <zhqkfxm0127@naver.com>
2023-11-14 13:23:50 +0900

    refactor(Event): 하드코딩 제거

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,7 +1,25 @@
 package christmas.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Event {
+    private static final int CHRISTMAS_DAY = 25;
+    private static final int CHRISTMAS_DEFAULT_DISCOUNT = 1000;
+    private static final List<Integer> WEEKEND = new ArrayList<>(
+            Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
+    );
+
     public int christmasDDay(int date) {
-        return 1000 + ((date - 1) * 100);
+        if (date <= CHRISTMAS_DAY) {
+            return CHRISTMAS_DEFAULT_DISCOUNT + ((date - 1) * 100);
+        }
+        return 0;
+    }
+
+    public int weekday(int date) {
+
+        return 0;
     }
 }
