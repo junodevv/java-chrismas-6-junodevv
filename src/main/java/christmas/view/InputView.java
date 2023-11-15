53de2b0a71e0cf6e92ae34007eb4b78667f7c695
leejuno <zhqkfxm0127@naver.com>
2023-11-15 23:05:18 +0900

    feat(InputView): 날짜에 대한 예외처리 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -17,8 +17,12 @@ public class InputView {
     }
 
     public int readDate() {
-        String date = readLine();
-        return Integer.parseInt(date);
+        String dateText = readLine();
+        int date = Integer.parseInt(dateText);
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR]유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        return date;
     }
 
     public void askMenu() {
