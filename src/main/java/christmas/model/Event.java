d290dee19aba3368502bc4bc39174066c57609a4
leejuno <zhqkfxm0127@naver.com>
2023-11-15 18:49:29 +0900

    feat(Event): 이벤트 조건에 맞는지 확인하는 기능 구현

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -2,11 +2,14 @@ package christmas.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Event {
+    private static final int EVENT_AMOUNT_CONDITION = 10000;
     private static final int CHRISTMAS_DAY = 25;
     private static final int CHRISTMAS_DEFAULT_DISCOUNT = 1000;
+    private static final int CHRISTMAS_ADDITIONAL_DISCOUNT = 100;
     private static final List<Integer> WEEKEND = new ArrayList<>(
             Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30)
     );
@@ -17,9 +20,16 @@ public class Event {
     private static final int STAR_DAY_DISCOUNT = 1000;
     private static final int PRESENT_CONDITION_AMOUNT = 120000;
 
+    public boolean checkCondition(int totalAmount, boolean checkMenu) {
+        if (totalAmount > EVENT_AMOUNT_CONDITION && checkMenu) {
+            return true;
+        }
+        return false;
+    }
+
     public int christmasDDay(int date) {
         if (date <= CHRISTMAS_DAY) {
-            return CHRISTMAS_DEFAULT_DISCOUNT + ((date - 1) * 100);
+            return CHRISTMAS_DEFAULT_DISCOUNT + ((date - 1) * CHRISTMAS_ADDITIONAL_DISCOUNT);
         }
         return 0;
     }
