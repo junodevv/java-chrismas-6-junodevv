2ece41abc2c76a0e43100f57ea2d46a587981725
leejuno <zhqkfxm0127@naver.com>
2023-11-15 21:54:39 +0900

    feat(Event): 배지 고르는 기능 구현

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -78,4 +78,17 @@ public class Event {
     public int finalPayment(int totalAmount, int totalBenefit) {
         return totalAmount - totalBenefit;
     }
+
+    public String checkBadge(int totalBenefit) {
+        if (totalBenefit >= 20000) {
+            return "산타";
+        }
+        if (totalBenefit >= 10000) {
+            return "트리";
+        }
+        if (totalBenefit >= 5000) {
+            return "별";
+        }
+        return null;
+    }
 }
