5d36be93ae3379278727f59c70c6aafd5d541712
leejuno <zhqkfxm0127@naver.com>
2023-11-15 17:46:48 +0900

    feat(OutputView): 증정메뉴 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -7,7 +7,10 @@ public class OutputView {
     private static final int EVENT_MONTH = 12;
     private static final String PREVIEW = EVENT_MONTH + "%d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     private static final String ORDER_MENU = "<주문 메뉴>";
-    private static final String BEFORE_DISCOUNT_AMOUNT = "<할인 전 총주문 금액>\n%d원"
+    private static final String BEFORE_DISCOUNT_AMOUNT = "<할인 전 총주문 금액>\n%d원";
+    private static final String PRESENT_MENU = "<증정 메뉴>";
+    private static final String PRESENT_TRUE = "샴페인 1개";
+    private static final String PRESENT_FALSE = "없음";
 
     public void preview(int date) {
         System.out.printf(PREVIEW, date);
@@ -24,4 +27,13 @@ public class OutputView {
         System.out.printf(BEFORE_DISCOUNT_AMOUNT, totalAmount);
     }
 
+    public void presentMenu(boolean present) {
+        System.out.println(PRESENT_MENU);
+        if (present==true) {
+            System.out.println(PRESENT_TRUE);
+        }
+        if (present==false) {
+            System.out.println(PRESENT_FALSE);
+        }
+    }
 }
