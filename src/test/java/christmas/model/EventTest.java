793653853a345104f0be6e97fb5e889e756b799f
leejuno <zhqkfxm0127@naver.com>
2023-11-15 16:30:38 +0900

    test(EventTest): 특별할인, 증정이벤트 기능 테스트

--- src/test/java/christmas/model/EventTest.java
+++ src/test/java/christmas/model/EventTest.java
@@ -43,7 +43,7 @@ class EventTest {
     @DisplayName("주말할인 이벤트 기능 테스트")
     @ParameterizedTest
     @CsvSource(value = {"2,7,0", "1,8,2023"})
-    void weekend(int value1, int value2, int expected) {
+    void weekend_테스트(int value1, int value2, int expected) {
         int mainCount = value1;
         int date = value2;
 
@@ -52,11 +52,25 @@ class EventTest {
         assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void starDay() {
+    @DisplayName("특별할인 이벤트 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"7,0", "10,1000"})
+    void starDay_테스트(int value, int expected) {
+        int date = value;
+
+        int actual = event.starDay(date);
+
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void present() {
+    @DisplayName("증정이벤트 기능 테스트")
+    @ParameterizedTest
+    @CsvSource(value = {"119000,false", "120000,true"})
+    void present_테스트(int value, boolean expected) {
+        int totalAmount = value;
+
+        boolean actual = event.present(totalAmount);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
