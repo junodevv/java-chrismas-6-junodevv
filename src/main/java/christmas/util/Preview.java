0e3f1fe7cb208236d90501a783874ba584b839c9
leejuno <zhqkfxm0127@naver.com>
2023-11-29 16:16:18 +0900

    refactor(Preview): 오타수정

--- src/main/java/christmas/util/Preview.java
+++ src/main/java/christmas/util/Preview.java
@@ -1,7 +1,7 @@
 package christmas.util;
 
 public enum Preview {
-    START("%d월%d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    START("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER_MENU("<주문 메뉴>"),
     BEFORE_DISCOUNT_AMOUNT("<할인 전 총주문 금액>"),
     PRESENT_MENU("<증정 메뉴>"),
